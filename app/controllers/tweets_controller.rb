4867ac5044452916c536d04a900df2f9890568f5
BrandonMathis <BeMathis@gmail.com>
2013-09-10 22:51:57 -0400

    Added tweet creation

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,19 @@
 class TweetsController < ApplicationController
+  before_filter :authenticate_user!, only: [:create, :new]
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    if @tweet = Tweet.create(body: params[:tweet][:body], user_id: current_user.id )
+      redirect_to tweets_path
+    else
+      render :new
+    end
+  end
+
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order('created_at DESC')
   end
 end
